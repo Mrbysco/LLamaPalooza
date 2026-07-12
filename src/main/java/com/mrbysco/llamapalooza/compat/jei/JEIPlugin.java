733d26cb8de7c3b3954c61d3ad98571fb8623504
Mrbysco <Mrbysco@users.noreply.github.com>
2026-07-12 15:21:25 +0200

    Re-enable JEI compat

--- src/main/java/com/mrbysco/llamapalooza/compat/jei/JEIPlugin.java
+++ src/main/java/com/mrbysco/llamapalooza/compat/jei/JEIPlugin.java
@@ -1,32 +1,51 @@
-//package com.mrbysco.llamapalooza.compat.jei;
-//
-//import com.mrbysco.llamapalooza.LlamaPalooza;
-//import com.mrbysco.llamapalooza.registry.LLamaRegistry;
-//import mezz.jei.api.IModPlugin;
-//import mezz.jei.api.JeiPlugin;
-//import mezz.jei.api.helpers.IGuiHelper;
-//import mezz.jei.api.helpers.IJeiHelpers;
-//import mezz.jei.api.registration.IRecipeCategoryRegistration;
-//import mezz.jei.api.registration.ISubtypeRegistration;
-//import net.minecraft.resources.ResourceLocation;
-//
-//@JeiPlugin
-//public class JEIPlugin implements IModPlugin {
-//	private static final ResourceLocation UID = LlamaPalooza.modLoc("jei_plugin");
-//
-//	@Override
-//	public ResourceLocation getPluginUid() {
-//		return UID;
-//	}
-//
-//	@Override
-//	public void registerCategories(IRecipeCategoryRegistration registration) {
-//		IJeiHelpers jeiHelpers = registration.getJeiHelpers();
-//		IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
-//	}
-//
-//	@Override
-//	public void registerItemSubtypes(ISubtypeRegistration registration) {
-//		registration.useNbtForSubtypes(LLamaRegistry.LOOT_LLAMA_SPAWN_EGG.get());
-//	}
-//}
+package com.mrbysco.llamapalooza.compat.jei;
+
+import com.mrbysco.llamapalooza.LlamaPalooza;
+import com.mrbysco.llamapalooza.registry.LLamaRegistry;
+import com.mrbysco.llamapalooza.registry.LlamaDataComponents;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.ingredients.subtypes.ISubtypeInterpreter;
+import mezz.jei.api.ingredients.subtypes.UidContext;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.ISubtypeRegistration;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+@JeiPlugin
+public class JEIPlugin implements IModPlugin {
+	private static final ResourceLocation UID = LlamaPalooza.modLoc("jei_plugin");
+
+	@Override
+	public ResourceLocation getPluginUid() {
+		return UID;
+	}
+
+	@Override
+	public void registerCategories(IRecipeCategoryRegistration registration) {
+	}
+
+	@Override
+	public void registerItemSubtypes(ISubtypeRegistration registration) {
+		registration.registerSubtypeInterpreter(VanillaTypes.ITEM_STACK, LLamaRegistry.LOOT_LLAMA_SPAWN_EGG.get(), new EggSubTypes());
+	}
+
+	private static class EggSubTypes implements ISubtypeInterpreter<ItemStack> {
+		@Override
+		@Nullable
+		public Object getSubtypeData(ItemStack ingredient, UidContext context) {
+			return ingredient.get(LlamaDataComponents.LOOT_TABLE);
+		}
+
+		@Override
+		public String getLegacyStringSubtypeInfo(ItemStack ingredient, UidContext context) {
+			ResourceLocation lootTable = ingredient.get(LlamaDataComponents.LOOT_TABLE);
+			if (lootTable == null) return "";
+			return BuiltInRegistries.ITEM.getKey(ingredient.getItem()) + "@" + lootTable;
+		}
+
+	}
+}
